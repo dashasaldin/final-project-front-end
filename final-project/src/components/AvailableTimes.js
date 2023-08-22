98cdd33122f734ba6ff286ef3ad570033be2b90d
dasha <dasha.saldin@gmail.com>
2023-08-22 20:56:45 +0200

    add form validation

--- final-project/src/components/AvailableTimes.js
+++ final-project/src/components/AvailableTimes.js
@@ -1,16 +1,17 @@
 import React from "react";
 
 export const AvailableTimes = (props) => {
-    return (
-        <select id="res-time"
-        value = {props.selectedTime}
-        onChange={e => props.setAvailableTime(e.target.value)}
-        >
-        {props.availableTimes.map(time =>
-            <option value={time} key={time}>
-                {time}
-            </option>
-           )}
-        </select>
-    )
-}
+  return (
+    <select
+      id="res-time"
+      value={props.selectedTime}
+      onChange={(e) => props.setAvailableTime(e.target.value)}
+    >
+      {props.availableTimes.map((time) => (
+        <option value={time} key={time}>
+          {time}
+        </option>
+      ))}
+    </select>
+  );
+};
--- final-project/src/components/BookingForm.js
+++ final-project/src/components/BookingForm.js
@@ -1,20 +1,46 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 import { AvailableTimes } from "./AvailableTimes.js";
 
+const DEFAULT_VALIDITY_STATE = {
+  isEmailValid: true,
+  isNameValid: true,
+};
+
+const namePattern = /^[A-Za-z\s]+$/;
+const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
 export const BookingForm = (props) => {
   const [date, setDate] = useState("");
-  const [numberOfGuests, setnumberOfGuests] = useState("1");
-  const [occasion, setOccasion] = useState("Birthday");
+  const [numberOfGuests, setnumberOfGuests] = useState("");
+  const [occasion, setOccasion] = useState("");
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
+  const [validityState, setValidityState] = useState(DEFAULT_VALIDITY_STATE);
+
+  useEffect(() => {
+    if (!namePattern.test(name)) {
+      setValidityState((old) => ({ ...old, isNameValid: false }));
+    }
+  }, [name]);
+
+  useEffect(() => {
+    if (!emailPattern.test(email)) {
+      setValidityState((old) => ({ ...old, isEmailValid: false }));
+    }
+  }, [email]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    if (!validityState.isEmailValid || !validityState.isNameValid) {
+      return;
+    }
+
     setDate("");
-    setnumberOfGuests("1");
+    setnumberOfGuests("");
     props.setAvailableTime("");
-    setOccasion("Birthday");
+    setOccasion("");
     setName("");
     setEmail("");
     props.submitForm(e);
@@ -28,15 +54,21 @@ export const BookingForm = (props) => {
           id="name"
           placeholder="Name"
           type="text"
+          autoComplete="off"
           value={name}
+          required
+          className={!validityState.isNameValid && "error-input"}
           onChange={(e) => setName(e.target.value)}
         />
         <label htmlFor="email">Email</label>
         <input
           id="email"
           placeholder="Email"
-          type="text"
+          type="email"
+          autoComplete="off"
           value={email}
+          className={!validityState.isEmailValid && "error-input"}
+          required
           onChange={(e) => setEmail(e.target.value)}
         />
         <label htmlFor="res-date">Choose date</label>
@@ -44,6 +76,7 @@ export const BookingForm = (props) => {
           id="res-date"
           type="date"
           value={date}
+          required
           onChange={(e) => {
             setDate(e.target.value);
             props.onDateChange(new Date(e.target.value));
@@ -56,22 +89,31 @@ export const BookingForm = (props) => {
         <input
           id="guests"
           type="number"
-          placeholder="1"
+          placeholder="Choose a number of guests"
           min="1"
           max="10"
           value={numberOfGuests}
+          required
           onChange={(e) => setnumberOfGuests(e.target.value)}
         />
         <label htmlFor="occasion">Occasion</label>
         <select
           id="occasion"
           value={occasion}
+          required
+          placeholder="Select occasion"
           onChange={(e) => setOccasion(e.target.value)}
         >
-          <option>Birthday</option>
-          <option>Anniversary</option>
+          <option value="">Select Occasion</option>
+          <option value="Birthday">Birthday</option>
+          <option value="Anniversary">Anniversary</option>
         </select>
-        <button type="submit">Make a reservation</button>
+        <button
+          type="submit"
+          disabled={!validityState.isEmailValid || !validityState.isNameValid}
+        >
+          Make a reservation
+        </button>
       </form>
     </div>
   );
