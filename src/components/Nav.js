7bd9c31da9595d64011e36c90e87169286627cde
dasha <dasha.saldin@gmail.com>
2024-11-13 15:26:35 +0100

    changes to folder name

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,21 +1,29 @@
-import React from "react"
-import { Link} from "react-router-dom";
+import React from "react";
+import { Link } from "react-router-dom";
 
 export const Nav = () => {
-
-    return (
-        <nav>
-        <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">About</Link></li>
-            <li><Link to="/menu">Menu</Link></li>
-            <li><Link to="/booking">Reservations</Link></li>
-            <li><Link to="/order-online">Order Online</Link></li>
-            <li><Link to="/login">Login</Link></li>
-        </ul>
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/menu">Menu</Link>
+        </li>
+        <li>
+          <Link to="/final-project-front-end/booking">Reservations</Link>
+        </li>
+        <li>
+          <Link to="/order-online">Order Online</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+      </ul>
     </nav>
-
-    )
-
-}
-
+  );
+};
